7eaf0eaae077960d395774864980a9effa2c86fd
Hensley <SHENSL12@ford.com>
2018-03-02 13:10:12 -0500

    Initial

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("I am determined to learn how to code.");
+        System.out.println("Today's date is");
+    }
+}
